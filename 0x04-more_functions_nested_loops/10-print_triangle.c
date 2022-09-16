d7ebb678e272d39b51f9d8264b5b41d1ea82f470
Royalwole <akolawoleakinola@gmail.com>
2022-09-15 20:59:23 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,9 +4,8 @@
  * print_triangle: output a triangle
  * @size: size of triangle
  *
- * Return: void
+ * always return 0
  */
-
 
 void print_triangle(int size)
 {
@@ -14,16 +13,20 @@ void print_triangle(int size)
 
 	if (size <= 0)
 		_putchar('\n');
+
 	for (a = 1; a <= size; a++)
 	{
 		for (b = 1; b <= size; b++)
 		{
 			if (b <= (size - a))
+
 				_putchar(' ');
 			else
 				_putchar('#');
 		}
 		_putchar('\n');
+
+		return(0);
 	}
 
 }
