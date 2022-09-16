447b64fe2a16399ff2805b54ae4a0145f3abe42c
Royalwole <akolawoleakinola@gmail.com>
2022-09-15 19:13:06 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - A function that prints the numbers from 0 to 9, followed by a new line.
- *increment c
- * Return: Always 0.
+ * print_numbers - prints 0123456789\n
+ *
+ * Return: void
  */
+
 
 void print_numbers(void)
 {
-	int c;
+	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
-
 	}
-
 	_putchar('\n');
 }
