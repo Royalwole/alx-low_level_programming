a82b35cd9cb5b71c04bde343c10c8b11737dd811
Royalwole <akolawoleakinola@gmail.com>
2022-09-19 19:04:49 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - This returns the length of a string
+ * @s: string to find the length 
+ *
+ * Return: length of s(string)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	return (i);
+}
