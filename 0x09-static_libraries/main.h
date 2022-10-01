ec6085de0cf9ae2b2285d6bca2e64b171feb1427
Royalwole <akolawoleakinola@gmail.com>
2022-09-30 20:22:34 -0700

    static library7

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,4 @@
 #!/bin/bash
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,5 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* _MAIN_H_ */
