5255a8e18f0cde29febef14ebbd048e6662b5341
Royalwole <akolawoleakinola@gmail.com>
2023-02-23 15:24:28 -0800

    let see

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,50 +1,22 @@
 #!/usr/bin/python3
+"""5-island_perimeter module"""
 
-"""
-This module contains a function that returns the perimeter of an island in a grid.
-"""
 
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in grid.
-    """
-    rows = len(grid)
-    cols = len(grid[0])
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                # Found the first land cell, do a depth-first search
-                visited = set()
-                perimeter = 0
-                dfs(row, col, visited, grid, perimeter)
-                return perimeter
+    """Returns the perimeter of the island described in grid"""
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
 
-def dfs(row, col, visited, grid, perimeter):
-    """
-    Does a depth-first search from the current cell (row, col) and marks all the
-    connected land cells as visited while counting the perimeter of the island.
-    """
-    rows = len(grid)
-    cols = len(grid[0])
-    visited.add((row, col))
-    # Check left
-    if col == 0 or grid[row][col - 1] == 0:
-        perimeter += 1
-    elif (row, col - 1) not in visited:
-        dfs(row, col - 1, visited, grid, perimeter)
-    # Check right
-    if col == cols - 1 or grid[row][col + 1] == 0:
-        perimeter += 1
-    elif (row, col + 1) not in visited:
-        dfs(row, col + 1, visited, grid, perimeter)
-    # Check up
-    if row == 0 or grid[row - 1][col] == 0:
-        perimeter += 1
-    elif (row - 1, col) not in visited:
-        dfs(row - 1, col, visited, grid, perimeter)
-    # Check down
-    if row == rows - 1 or grid[row + 1][col] == 0:
-        perimeter += 1
-    elif (row + 1, col) not in visited:
-        dfs(row + 1, col, visited, grid, perimeter)
-
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+    return c
