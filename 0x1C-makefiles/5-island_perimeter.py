0ec45fb2e0491786324589c5179c216f842f7767
Royalwole <akolawoleakinola@gmail.com>
2023-02-23 15:18:53 -0800

    interview prep

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,37 +1,50 @@
 #!/usr/bin/python3
+
 """
-This module contains a function island_perimeter
+This module contains a function that returns the perimeter of an island in a grid.
 """
 
 def island_perimeter(grid):
-"""
-Returns the perimeter of the island described in grid
-Args:
-    grid (list[list[int]]): a rectangular grid of 0s and 1s
+    """
+    Returns the perimeter of the island described in grid.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Found the first land cell, do a depth-first search
+                visited = set()
+                perimeter = 0
+                dfs(row, col, visited, grid, perimeter)
+                return perimeter
 
-Returns:
-    int: the perimeter of the island in the grid
+def dfs(row, col, visited, grid, perimeter):
+    """
+    Does a depth-first search from the current cell (row, col) and marks all the
+    connected land cells as visited while counting the perimeter of the island.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    visited.add((row, col))
+    # Check left
+    if col == 0 or grid[row][col - 1] == 0:
+        perimeter += 1
+    elif (row, col - 1) not in visited:
+        dfs(row, col - 1, visited, grid, perimeter)
+    # Check right
+    if col == cols - 1 or grid[row][col + 1] == 0:
+        perimeter += 1
+    elif (row, col + 1) not in visited:
+        dfs(row, col + 1, visited, grid, perimeter)
+    # Check up
+    if row == 0 or grid[row - 1][col] == 0:
+        perimeter += 1
+    elif (row - 1, col) not in visited:
+        dfs(row - 1, col, visited, grid, perimeter)
+    # Check down
+    if row == rows - 1 or grid[row + 1][col] == 0:
+        perimeter += 1
+    elif (row + 1, col) not in visited:
+        dfs(row + 1, col, visited, grid, perimeter)
 
-"""
-rows = len(grid)
-cols = len(grid[0])
-
-perimeter = 0
-
-for i in range(rows):
-    for j in range(cols):
-        if grid[i][j] == 1:
-            # Check left side
-            if j == 0 or grid[i][j-1] == 0:
-                perimeter += 1
-            # Check right side
-            if j == cols-1 or grid[i][j+1] == 0:
-                perimeter += 1
-            # Check top side
-            if i == 0 or grid[i-1][j] == 0:
-                perimeter += 1
-            # Check bottom side
-            if i == rows-1 or grid[i+1][j] == 0:
-                perimeter += 1
-
-return perimeter
